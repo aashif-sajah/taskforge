dbbae3328b2b64a087d4bd732f8c1c8678f2c4db
Aashif Sajah <masaashifsajah@gmail.com>
2025-04-16 22:48:39 +0530

    Updated the TaskRepo

--- taskforge/src/main/java/com/aashif/taskforge/repo/TaskRepo.java
+++ taskforge/src/main/java/com/aashif/taskforge/repo/TaskRepo.java
@@ -1,8 +1,13 @@
 package com.aashif.taskforge.repo;
 
 import com.aashif.taskforge.model.Task;
+import com.aashif.taskforge.model.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface TaskRepo extends JpaRepository<Task, Integer> {}
+public interface TaskRepo extends JpaRepository<Task, Long> {
+    List<Task> findByStatus(TaskStatus status);
+}
